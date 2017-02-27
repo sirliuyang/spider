9bf057d18b58fef5c898f48d9ffb5f2853e85d44
sirliuyang@hotmail.com <sirliuyang@hotmail.com>
2017-02-27 21:48:15 +0800

    Update Starter and Mongo Dao

--- spider/src/main/java/com/formula/spider/Starter.java
+++ spider/src/main/java/com/formula/spider/Starter.java
@@ -8,8 +8,7 @@ package com.formula.spider;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.formula.spider.dao.RestaurantRepository;
-import com.formula.spider.processor.SmzdmPageProcessor;
+import com.formula.spider.dao.MongoRepository;
 import com.formula.spider.util.TraverseAllCityForDianping;
 
 import us.codecraft.webmagic.Spider;
@@ -18,10 +17,11 @@ public class Starter {
 	private static String dianping = "http://www.dianping.com/citylist/";
 	public static void main(String[] args){
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-		RestaurantRepository restaurantRepository = context.getBean(RestaurantRepository.class);
-		TraverseAllCityForDianping getter = new TraverseAllCityForDianping();
+		MongoRepository mongoRepository = context.getBean(MongoRepository.class);
+		mongoRepository.test();
+/*		TraverseAllCityForDianping getter = new TraverseAllCityForDianping();
 		Spider test = new Spider(getter);
 		test.test(dianping);
-		System.out.println(getter.getCities());
+		System.out.println(getter.getCities());*/
 	}
 }
--- spider/src/main/java/com/formula/spider/dao/MongoRepository.java
+++ spider/src/main/java/com/formula/spider/dao/MongoRepository.java
@@ -3,17 +3,31 @@
  */
 package com.formula.spider.dao;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 /**
  * @author Leon(Liu Yang) Ideas from Formula Innovation
  */
+@Repository
 public class MongoRepository {
 	@Autowired
 	private MongoTemplate mongoTemplate;
 
-	public boolean insert() {
+	public boolean BasicDBObjectBuilderInsert(String database, String table) {
 		return false;
+	}
+	
+	public void test(){
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase database = mongoClient.getDatabase("dianping");
+		MongoCollection<Document> collection = database.getCollection("restaurant");
+		System.out.println(collection.toString());
 	}
 }
